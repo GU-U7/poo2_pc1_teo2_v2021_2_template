995af2de06cae504dc15461b92472f465dcd2999
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-10-12 20:38:50 -0500

    Add files via upload

--- src/P1.cpp
+++ src/P1.cpp
@@ -1,12 +1,34 @@
-//
-// Created by rudri on 10/11/2020.
-//
+#include "p1.h"
 
-#include <iostream>
-#include "P1.h"
-using namespace std;
+join_vectors_t::join_vectors_t(initializer_list<int> il1, initializer_list<int> il2):l1(distance(begin(il1),end(il1))), l2(distance(begin(il2), end(il2))){
+	arr = new int[l1+l2];
+	vector<int>_temp(begin(il1), end(il1));
+	for(auto i: il2) _temp.push_back(i);
+	sort(_temp.begin(), _temp.end());
+	copy(_temp.begin(), _temp.end(), arr);
+}
+join_vectors_t::join_vectors_t(vector<int> v1, vector<int> v2):l1(v1.size()), l2(v2.size()){
+	arr=new int[l1+l2];
+	vector<int>_temp = v1;
+	for(auto i: v2) _temp.push_back(i);
+	sort(_temp.begin(), _temp.end());
+	copy(_temp.begin(), _temp.end(), arr);
+}
 
+join_vectors_t::~join_vectors_t(){
+	
+	delete[] arr;
+}
 
-void question_1(){
+size_t join_vectors_t::size(){
+	return l1+l2;
+}
 
+int* join_vectors_t::operator!(){
+	reverse(arr, arr+l1+l2);
+	return arr;
+}
+
+int* join_vectors_t::operator()(){
+	return arr;
 }
--- src/P1.h
+++ src/P1.h
@@ -6,6 +6,33 @@
 #define POO2_PC1_TEO2_V2021_2_P1_H
 
 
-void question_1();
+#ifndef p1_h
+#define p1_h
+
+#include<initializer_list>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#include<iterator>
+
+using namespace std;
+
+class join_vectors_t{
+	public:
+	join_vectors_t()=default;
+	join_vectors_t(initializer_list<int> il1, initializer_list<int> il2);
+	join_vectors_t(vector<int> v1, vector<int> v2);
+	~join_vectors_t();
+
+	size_t size();
+
+	int* operator!();
+	int* operator()();	
+	private:
+	int *arr;
+	size_t l1,l2;
+};
+
+#endif
 
 #endif //POO2_PC1_TEO2_V2021_2_P1_H
